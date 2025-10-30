69e408172e724ef7437c3839ef9890a4fcfcd01b
PHONE-BOOTH-H <hantaeyoung9@ajou.ac.kr>
2025-10-30 16:43:04 +0900

    dev: disable StrictMode in development to prevent double effects (fix 20-point score issue)

--- src/index.tsx
+++ src/index.tsx
@@ -14,8 +14,14 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (process.env.NODE_ENV === 'development') {
+  // 개발 모드에서는 StrictMode의 이중 호출로 인해 게임 루프/점수 증가가
+  // 두 번 실행되는 혼란을 방지하기 위해 StrictMode를 비활성화한다.
+  root.render(<App />);
+} else {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
